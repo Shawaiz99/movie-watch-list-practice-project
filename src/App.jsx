f1afe16bb2bd18f01c22b85fd4096431bd5d9a6a
Shawaiz <shawaizy2k@gmail.com>
2025-02-17 17:01:38 -0500

    Making the basic layouts of the card project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
+import MovieCard from './components/MovieCard';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
       <Navbar />
+      <MovieCard />
     </>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,13 @@
 function MovieCard() {
   return (
-    <div>
-      
+    <div className="col-6 m-5">
+      <div className="card bg-bone-white text-dark">
+        <div className="card-body">
+            <h3 className="card-title m-0">
+                Card
+            </h3>
+        </div>
+      </div>
     </div>
   )
 }
