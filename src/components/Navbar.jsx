0720841031e2f7ed6e378ef26b01c42b1158c5fe
Shawaiz <shawaizy2k@gmail.com>
2025-02-16 20:01:13 -0500

    Finished Navbar. Moving onto MovieCard

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 function Navbar() {
   return (
     <nav className=" navbar bg-body-tertiary">
-      zug
+      <div className="container">
+        <div className="navbar-brand fs-3 fw-bold text-primary text-opacity-75 mx-auto">
+          Movie Watch List
+        </div>
+      </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
