887765617e8c59f0e6fd15d656e0e684cc02cc32
Shawaiz <shawaizy2k@gmail.com>
2025-02-20 23:45:58 -0500

    Added badge to the Card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,20 +1,25 @@
-import { FaTrashCan } from "react-icons/fa6"
+import { FaTrashCan } from "react-icons/fa6";
 
 function MovieCard() {
   return (
     <div className="col-6 m-5">
-      <div className="card bg-bone-white text-dark">
+      <div className="card bg-bone-white text-dark position-relative">
+        <span
+          className="position-absolute top-0 start-0 translate-middle badge bg-danger
+        custom-badge-position"
+        >
+          99+
+        </span>
+
         <div className="card-body d-flex justify-content-between">
-            <h3 className="card-title m-0">
-                Card
-            </h3>
-            <div className="text-danger">
-              <FaTrashCan/>
-            </div>
+          <h3 className="card-title m-0">Inception</h3>
+          <div className="text-danger">
+            <FaTrashCan />
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
