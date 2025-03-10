bcbf33346aa32562ab5d4825ad13927053598171
Shawaiz <shawaizy2k@gmail.com>
2025-03-10 03:58:12 -0400

    Deleted Test Data

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,7 @@ import { useState } from "react";
 
 function App() {
   
-  const [movies, setMovies] = useState([
-    {
-      id: 1001,
-      title: "Inception",
-      rate: 8.7,
-    },
-    {
-      id: 1002,
-      title: "Rush Hour",
-      rate: 7.7,
-    },
-    {
-      id: 1003,
-      title: "Lord of the Rings: Fellowship of the Ring",
-      rate: 9.0,
-    }
-  ]);
+  const [movies, setMovies] = useState([]);
   
   const addMovie = (newMovie) => {
     // movies.push(newMovie);
